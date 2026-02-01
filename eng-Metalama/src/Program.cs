19eb88a36dbc778a9888fb2ee2778bb553725e33
Gael Fraiteur <gael@postsharp.net>
2026-02-01 08:15:52 +0100

    Update eng.

--- eng-Metalama/src/Program.cs
+++ eng-Metalama/src/Program.cs
@@ -16,7 +16,7 @@ var product = new Product(MetalamaDependencies.MetalamaCompiler)
         Components =
         [
             // Must match global.json.
-            new DotNetComponent("9.0.201", DotNetComponentKind.Sdk),
+            new DotNetComponent("9.0.310", DotNetComponentKind.Sdk),
             new VisualStudioBuildToolsComponent(
                 VisualStudioBuildToolsComponentVersion.v17_14_15,
             [
