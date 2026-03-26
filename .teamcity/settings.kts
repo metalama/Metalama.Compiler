505dc67fc368ef870ae7031498e7d64bbedf56aa
Gael Fraiteur <gael@postsharp.net>
2026-03-26 15:29:07 +0100

    Update eng.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -8,7 +8,7 @@ import jetbrains.buildServer.configs.kotlin.failureConditions.*
 import jetbrains.buildServer.configs.kotlin.triggers.*
 import jetbrains.buildServer.configs.kotlin.projectFeatures.*
 
-version = "2025.07"
+version = "2025.11"
 
 project {
 
@@ -425,7 +425,7 @@ object UpstreamMerge : BuildType({
                 path = "DockerBuild.ps1"
             }
             noProfile = false
-            scriptArgs = "-BuildImage -ImageName metalamacompiler-2026.0 -Dockerfile Dockerfile.claude "
+            scriptArgs = "-BuildImage -ImageName metalamacompiler-2026.0 -Dockerfile eng-Metalama/docker/Dockerfile.claude "
         }
         powerShell {
             name = "Merge upstream"
@@ -435,7 +435,7 @@ object UpstreamMerge : BuildType({
                 path = "DockerBuild.ps1"
             }
             noProfile = false
-            scriptArgs = "-Script Build.ps1 -ImageName metalamacompiler-2026.0 -Dockerfile Dockerfile.claude -NoBuildImage -Label %system.teamcity.buildType.id%_%build.number% -Snapshot upstream-merge --timeout %UpstreamMerge.Timeout% %UpstreamMerge.Arguments%"
+            scriptArgs = "-Script Build.ps1 -ImageName metalamacompiler-2026.0 -Dockerfile eng-Metalama/docker/Dockerfile.claude -NoBuildImage -Label %system.teamcity.buildType.id%_%build.number% -Snapshot upstream-merge --timeout %UpstreamMerge.Timeout% %UpstreamMerge.Arguments%"
         }
         powerShell {
             name = "Cleanup Docker containers"
